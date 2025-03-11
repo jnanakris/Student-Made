ead1c2df87d223d3683ebe10cd60cf5ad72dbc71
JesusGG03 <jesusgg2704@gmail.com>
2025-03-11 18:32:37 -0400

    Feat: Added basic styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>Header</div>
+        <div className='px-10 flex justify-between items-center h-[10vh] bg-(--niner-green) text-white'>
+            <h1 className='text-4xl'>Niner Mine</h1>
+
+            <div className='flex items-center grow max-w-[200px] justify-between'>
+                <p>About</p>
+                <p>Shop</p>
+                <p>Join/Login</p>
+            </div>
+        </div>
     )
+
+
 }
 
 export default Header
