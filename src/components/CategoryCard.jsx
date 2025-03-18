a934d5fe1a8431aea5a99f3e14033a6ff6fe7b35
JesusGG03 <jesusgg2704@gmail.com>
2025-03-18 18:51:34 -0400

    Feat: Changed the dimensions of the card

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,7 +3,7 @@ import RingImage from "../assets/example-image.jpg"
 
 function CategoryCard({ imgSrc, category}) {
     return (
-        <div className='w-60 h-100 relative rounded-xl overflow-hidden shadow-lg cursor-pointer mx-[2vh]'>
+        <div className='w-45 h-80 relative rounded-xl overflow-hidden shadow-lg cursor-pointer mx-[2vh]'>
             <img src={RingImage} alt="Ring example image" className='w-full h-full object-cover'/>
             <p className='absolute inset-0 flex items-center justify-center text-white text-3xl font-bold outlined-text'>
                 Rings
