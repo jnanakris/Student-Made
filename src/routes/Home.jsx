81a9e3c2490987fe7965dfead65c5ee7582fed73
JesusGG03 <jesusgg2704@gmail.com>
2025-03-21 19:21:36 -0400

    Comment: Added comments that will dictate the order of sections

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,14 +4,29 @@ function Home() {
     return (
         <div className='flex flex-col'>
             
+            {/* Hero/poster when user first opens the website */}
+
             <div className='w-screen h-[55vh] bg-black text-white flex items-center justify-center'>
                 placeholder
             </div>
+        
+            {/* Category section, will contain category cards. horizontal scroll */}
 
+            <div className=''>
 
+            </div>
 
+            {/* Featured items that are being sold. Could use the creator bubble for this */}
 
+            <div className=''>
 
+            </div>
+
+            {/* Vendors sections. Horizontal scroll that includes all the vendors available */}
+
+            <div className=''>
+
+            </div>
 
         </div>
     )
