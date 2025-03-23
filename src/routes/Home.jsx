7bbe438898efeca96b3331d48e550fbe52345d68
JesusGG03 <jesusgg2704@gmail.com>
2025-03-22 21:16:11 -0400

    Feat: Added the Category section, updated the width of the category card

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react' 
+import CategoryCard from '../components/CategoryCard'
 
 function Home() {
     return (
@@ -6,13 +7,23 @@ function Home() {
             
             {/* Hero/poster when user first opens the website */}
 
-            <div className='w-screen h-[55vh] bg-black text-white flex items-center justify-center'>
-                placeholder
+            <div className='w-screen h-[80vh] bg-black text-white flex items-center justify-center'>
+                
+                {/* Buttons that can lead to shopping page */}
+
             </div>
         
             {/* Category section, will contain category cards. horizontal scroll */}
 
-            <div className=''>
+            <div className='w-screen h-[60vh] bg-(--niner-gold) flex flex-col '>
+
+                <p className='py-15 text-6xl text-white '>
+                    Shop By Category
+                </p>
+
+                <div className=''>
+                    <CategoryCard/>
+                </div>
 
             </div>
 
